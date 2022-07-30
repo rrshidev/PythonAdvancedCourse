c5ff7143e5d94da32443e7ac391f6371ea230f19
Ruslan <92475106+rrshidev@users.noreply.github.com>
2022-07-30 19:19:35 +0300

    Delete 2-2-5.py

--- 2-2-5.py
+++
@@ -1,7 +0,0 @@
-stringInput = input().split(' ')
-resultList, resultList_ = [], []
-for i in stringInput:
-    resultList.append(i)
-for i in range(1):
-        resultList.insert(0, resultList.pop())
-print(*resultList)
